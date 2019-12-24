701349eea28191ae9fd3c5fd122fed8d0f83f869
Billy Galloway <b.galloway@f5.com>
2019-12-24 00:46:13 -0800

    added zsh

--- tools/update_profile.sh
+++ tools/update_profile.sh
@@ -1,9 +1,19 @@
-# add this to your shell profile
-
+# add this to your bash shell profile
 function git(){
  if [[ $1 == "role" ]]
  then
     read -p "Enter role name: " ROLE_NAME
+    export ROLE_NAME
+ else
+    /usr/bin/git $@
+ fi
+}
+
+# For zshell on MacOS
+git(){
+ if [[ $1 == "role" ]]
+ then
+    vared -p "Enter role name: " ROLE_NAME
     export ROLE_NAME
  else
     /usr/bin/git $@
